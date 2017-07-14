d9436331d93286cad9f3ed41a504de667a7a1070
DGAH <1346952149@qq.com>
2017-07-14 22:28:40 +0800

    fix java.lang.NullPointerException
    update sth. about HBaseConfiguration

--- src/main/scala/HBase.scala
+++ src/main/scala/HBase.scala
@@ -1,6 +1,7 @@
 import scala.collection.JavaConverters._
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.hbase.client._
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable
 import org.apache.hadoop.hbase.{HBaseConfiguration, TableName}
 
 /**
@@ -36,14 +37,24 @@ object HBase {
    */
   def commit(data: Put, tableName: String): Boolean = {
     try {
-      val table = new HTable(m_HBase_Config, TableName.valueOf(tableName))
+      val hConfig = HBaseConfiguration.create()
+      Config.m_HBase_Params.foreach(
+        kv_pair => {
+          hConfig.set(kv_pair._1, kv_pair._2)
+        }
+      )
+      val table = new HTable(hConfig, TableName.valueOf(tableName))
+      //val table = new HTable(m_HBase_Config, TableName.valueOf(tableName))
       table.put(data)
       table.flushCommits()
       table.close()
     } catch {
       case ex: Exception => {
         if (Config.DebugMode) {
+          println("****************ERROR:BEGIN****************")
           println(ex.toString)
+          ex.printStackTrace()
+          println("****************ERROR:END****************")
         }
         return false
       }
@@ -55,14 +66,24 @@ object HBase {
    */
   def commit(puts: List[Put], tableName: String): Boolean = {
     try {
-      val table = new HTable(m_HBase_Config, TableName.valueOf(tableName))
+      val hConfig = HBaseConfiguration.create()
+      Config.m_HBase_Params.foreach(
+        kv_pair => {
+          hConfig.set(kv_pair._1, kv_pair._2)
+        }
+      )
+      val table = new HTable(hConfig, TableName.valueOf(tableName))
+      //val table = new HTable(m_HBase_Config, TableName.valueOf(tableName))
       table.put(puts.asJava)
       table.flushCommits()
       table.close()
     } catch {
       case ex: Exception => {
         if (Config.DebugMode) {
+          println("****************ERROR:BEGIN****************")
           println(ex.toString)
+          ex.printStackTrace()
+          println("****************ERROR:END****************")
         }
         return false
       }
@@ -75,13 +96,23 @@ object HBase {
   def acquire(ask: Get, tableName: String): Result = {
     var result: Result = null
     try {
-      val table = new HTable(m_HBase_Config, TableName.valueOf(tableName))
+      val hConfig = HBaseConfiguration.create()
+      Config.m_HBase_Params.foreach(
+        kv_pair => {
+          hConfig.set(kv_pair._1, kv_pair._2)
+        }
+      )
+      val table = new HTable(hConfig, TableName.valueOf(tableName))
+      //val table = new HTable(m_HBase_Config, TableName.valueOf(tableName))
       result = table.get(ask)
       table.close()
     } catch {
       case ex: Exception => {
         if (Config.DebugMode) {
+          println("****************ERROR:BEGIN****************")
           println(ex.toString)
+          ex.printStackTrace()
+          println("****************ERROR:END****************")
         }
       }
     }
@@ -96,16 +127,27 @@ object HBase {
   def acquire(gets: List[Get], tableName: String): Array[Result] = {
     var result: Array[Result] = Array[Result]()
     try {
-      val table = new HTable(m_HBase_Config, TableName.valueOf(tableName))
+      val hConfig = HBaseConfiguration.create()
+      Config.m_HBase_Params.foreach(
+        kv_pair => {
+          hConfig.set(kv_pair._1, kv_pair._2)
+        }
+      )
+      val table = new HTable(hConfig, TableName.valueOf(tableName))
+      //val table = new HTable(m_HBase_Config, TableName.valueOf(tableName))
       result = table.get(gets.asJava)
       table.close()
     } catch {
       case ex: Exception => {
         if (Config.DebugMode) {
+          println("****************ERROR:BEGIN****************")
           println(ex.toString)
+          ex.printStackTrace()
+          println("****************ERROR:END****************")
         }
       }
     }
     result
   }
+
 }
